c2e9d743cb0ee247dca0aa79b059317091df4fdb
Zachary Lumpkins <zlumpkins@blizzard.com>
2021-11-10 22:47:29 -0800

    fix(): some stuff

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  basePath: '/pachi-patch',
-  assetPrefix: '/pachi-patch/',
   reactStrictMode: true,
   images: {
     domains: ['cdn.discordapp.com'],
